c5f6ebfb3ea5a57eda4b90b3850b863b572d5198
redbobcat <durnov.jr@gmail.com>
2016-01-10 00:07:03 +0300

    All working now, needs to fix interrupts, counter breaks main PC interrupt may be.

--- main.c
+++ main.c
@@ -50,52 +50,82 @@
 #include "usart.h"
 
 
-inline void 	bldc_out_init (void); //bldc out port configure, output, sets to 0
 
-inline void 	bldc_hall_init (void); //bldc hall in configure, input, hi-z
-
+//funcs for bldc drive
 inline uint8_t 	bldc_hall_state (void); //returns hall sensor state
-
 void 			bldc_out_set (uint8_t); //closes all gates and sets all to next
-
 uint8_t 		bldc_switch (uint8_t hallin, uint8_t direction); //2 for CCW, 0 for none, 1 for CW
-
 const uint8_t 	bldc_state[] = {0b00100001, 0b00001001, 0b00011000, 0b00010010, 0b00000110, 0b00100100}; //output table for switching
+inline void 	bldc_out_init (void); //bldc out port configure, output, sets to 0
+inline void 	bldc_hall_init (void); //bldc hall in configure, input, hi-z
+void 			bldc_pwm_enable (void); //pwm enabling on PD6 pin, ~31KHz
+#define PWM_POWER OCR0A
 
-inline void 	bldc_start (uint8_t); //2 for CCW, 0 for none, 1 for CW
-
-inline void 	bldc_stop (void);
-
-
-//enable pinchange interrupt for PB0-PB2
+	//enable pinchange interrupt for PB0-PB2
 inline void		bldc_interupt_enable (void);
 inline void		bldc_interupt_disable (void);
-// ISR of pinchange interupt is in the end of file
+	// ISR of pinchange interupt is in the end of file
 
-void 			bldc_pwm_enable (void);
-//#define PWM_DRIVE OCR0A;
+
+
+
+//top funcs for bldc
+inline void 	bldc_start (uint8_t); //2 for CCW, 0 for none, 1 for CW
+inline void 	bldc_stop (void);
+inline void 	bldc_all_init (void); //all init in one
+
+
+//counter for serving others, 50hz
+void counter2_init (void);
+//ISR is in the end
+//variables for counter
+volatile uint8_t one_sec=50,ten_sec=5;
+
+
+//ACD functions
+void ADC_init (void); // init of ADC
+void ADC_start (uint8_t); // starts converson on ch
+
+volatile uint8_t adc_ch[2];
+volatile uint8_t adc_ch_now=6;
+
+
 
 //temp section for debug
-uint8_t dir=1, pwm=255;
+uint8_t dir=1, pwm=0;
 
 void main(void)
 {
 	USART_Init(MYUBRR);
+		
+	bldc_all_init();
 	
-	bldc_hall_init();
+	ADC_init();
+	counter2_init();
 	
-	bldc_out_init();
-	
-	bldc_pwm_enable();
-	
-	OCR0A = pwm;
-	bldc_interupt_enable();
 	sei();
+	
+	bldc_start(dir);
+	PWM_POWER=200;
+	
 	
 	for (;;)
 		{
-			_delay_ms(20);
+			PWM_POWER=adc_ch[1];
+			USART_Transmit('P');
+			
+			USART_Transmit(0x30+adc_ch[1]/100);
+			USART_Transmit(0x30+((adc_ch[1]%100)/10));
+			USART_Transmit(0x30+adc_ch[1]%10);
+			USART_Transmit('I');
+			USART_Transmit(0x30+adc_ch[0]/100);
+			USART_Transmit(0x30+((adc_ch[0]%100)/10));
+			USART_Transmit(0x30+adc_ch[0]%10);
+			
+					
+			
 		}
+	
 	
 	
 		
@@ -124,21 +154,37 @@ void bldc_out_set (uint8_t in)
 
 uint8_t bldc_switch (uint8_t hallin, uint8_t direction)
 	{	uint8_t state_temp=0;
+		
+		
 		if (direction==0)
 			{
 				return 0;
 			}
-		switch (hallin)
+		if (direction==1) //CW
 			{
+		
+		switch (hallin)
+				{
+				case 0b00000101: {state_temp=bldc_state[4+direction-1]; break;}
+				case 0b00000001: {state_temp=bldc_state[5+direction-1]; break;}
+				case 0b00000011: {state_temp=bldc_state[0+direction-1]; break;}
+				case 0b00000010: {state_temp=bldc_state[1+direction-1]; break;}
+				case 0b00000110: {state_temp=bldc_state[2+direction-1]; break;}
+				case 0b00000100: {state_temp=bldc_state[3+direction-1]; break;}
+				}
+			}
+			
+		if (direction==2) //CCW must be changed!!! CHANGED, test it!
+			{
+				switch (hallin)
+				{
 				case 0b00000101: {state_temp=bldc_state[0+direction-1]; break;}
 				case 0b00000001: {state_temp=bldc_state[1+direction-1]; break;}
 				case 0b00000011: {state_temp=bldc_state[2+direction-1]; break;}
 				case 0b00000010: {state_temp=bldc_state[3+direction-1]; break;}
 				case 0b00000110: {state_temp=bldc_state[4+direction-1]; break;}
-				case 0b00000100: 
-					{ if (direction == 1) state_temp=bldc_state[5+direction-1];
-						else state_temp=bldc_state[0];
-					}
+				case 0b00000100: {state_temp=bldc_state[5+direction-1]; break;}
+				}
 			}
 				
 		return state_temp;	
@@ -147,8 +193,7 @@ uint8_t bldc_switch (uint8_t hallin, uint8_t direction)
 inline void bldc_start (uint8_t cwccw)
 	{
 			PHASE_PORT=bldc_switch(bldc_hall_state(),cwccw);
-			OCR0A = 125;
-			//_delay_ms(20);
+			OCR0A = 200;
 			bldc_interupt_enable();
 			sei();
 	}
@@ -185,4 +230,61 @@ void bldc_pwm_enable (void)
 			DDRD |= 1<<PD6;
 			TCCR0B |= 1<<CS00; //start at full speed
 			
+	}
+
+inline void bldc_all_init(void)
+	{
+		bldc_hall_init();
+		bldc_out_init();
+		bldc_pwm_enable();
+		
+	}
+	
+void counter2_init (void)
+	{
+		TCCR2A |= (1<<WGM21); //CTC mode
+		TCCR2B |= (7<<CS20); //prescaler /1024
+		OCR2A=234;  //234 gives 50 ints/s
+		TIMSK2 |= (1<<OCIE2A); //enabling A interupt
+	}
+	
+ISR (TIMER2_COMPA_vect) //50 times/s
+	{
+			if (one_sec==0)
+				{
+					USART_Transmit('t');
+					one_sec=51;
+				}
+			if (ten_sec==0)
+				{
+						if (adc_ch_now==6)
+							{
+								adc_ch[0] = ADCH;
+								adc_ch_now=7;
+								ADC_start(adc_ch_now); 
+							}
+						else
+							{
+								adc_ch[1] = ADCH;
+								adc_ch_now=6;
+								ADC_start(adc_ch_now); 
+							}
+						
+						ten_sec=6;
+				}
+			
+			ten_sec--;
+			one_sec--;
+			sei();
+	}
+	
+void ADC_init (void)
+	{
+		ADMUX |= (1<<REFS0) | (1<<ADLAR); //ref to VCC, left adjust
+		ADCSRA |= (1<<ADEN) | (6<<ADPS0); //enabling ADC, pescaling /64, 180Khz
+	}
+void ADC_start (uint8_t chno)
+	{
+		ADMUX = (ADMUX & 0b11110000) | (chno); //set chanel
+		ADCSRA |= (1<<ADEN) | (1<<ADSC);
 	}
